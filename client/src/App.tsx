afd84552dd820d20aa27560f3e83b802d109f946
saiumesh535 <d.sai535@gmail.com>
2020-01-14 19:18:44 +0530

    types corrected

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './App.css';
 import ResolverComponent from './components/resolver/resolver.component';
 
-const App: React.FC = () => {
+const App: FC = () => {
   return (
     <div className="App">
       <ResolverComponent />
